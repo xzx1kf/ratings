26cba227edebae2091ec18f873beefc0d1bf4eab
Nick Hale <nicholas.hale@gmail.com>
2017-01-20 10:49:01 +0000

    added league selection

--- football/forms.py
+++ football/forms.py
@@ -5,3 +5,9 @@ class AnalysisForm(forms.Form):
     premiership = Division.objects.filter(name='E0')
     home_team = forms.ModelChoiceField(queryset=Team.objects.filter(division=premiership), empty_label=None)
     away_team = forms.ModelChoiceField(queryset=Team.objects.filter(division=premiership), empty_label=None)
+
+class LeagueForm(forms.Form):
+    division = forms.ModelChoiceField(
+            queryset=Division.objects.all(),
+            empty_label=None,
+    )
